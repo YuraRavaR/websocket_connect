6fee0b31004c7b36b7c0f1f6cb0384851f09909c
Yura Kochubailo <yurii.kochubailo@chisw.com>
2023-12-11 12:18:08 +0100

    add java doc

--- src/main/java/websocket/SocketContext.java
+++ src/main/java/websocket/SocketContext.java
@@ -5,23 +5,46 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * SocketContext class represents configuration for WebSocket connection
+ */
 public class SocketContext {
 
+    /**
+     * URI for the WebSocket connection
+     */
     private String URI;
-
+    /**
+     * expected message to verify if received
+     */
     private String expectedMessage;
-
+    /**
+     * custom request headers for WebSocket connection
+     */
     private Map<String, String> requestHeaders = new HashMap<>();
-
+    /**
+     * list to store received messages
+     */
     private List<String> messageList = new ArrayList<>();
-
+    /**
+     * status code of WebSocket connection
+     */
     private int statusCode;
-
-    private int timeOut = 10;
+    /**
+     * timeout for WebSocket connection
+     */
+    private int timeOut;
+    /**
+     * time taken for connection to remain alive
+     */
     private int timeTaken;
-
+    /**
+     * body to be sent as message to WebSocket server
+     */
     private String body;
-
+    /**
+     * runnable task to be executed during WebSocket connection
+     */
     private Runnable runnable;
 
     public void setURI(String URI) {
