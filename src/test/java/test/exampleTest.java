98ab85d5bfe9101954b237f85b83f28909fe74b5
Yura Kochubailo <yurii.kochubailo@chisw.com>
2023-12-11 11:17:11 +0100

    move websocket package to main

--- src/test/java/test/exampleTest.java
+++ src/test/java/test/exampleTest.java
@@ -9,8 +9,7 @@ public class exampleTest {
     @Test
     public void sendMessageAndVerify() {
         SocketContext context = new SocketContext();
-//        String url = "wss://free.blr2.piesocket.com/v3/1?api_key=TKuLcaxJt7IeqA4Qkzxt2J3WbKC8Gg93Ng9VOg1Q&notify_self=1";
-        String url = "";
+        String url = "wss://free.blr2.piesocket.com/v3/1?api_key=TKuLcaxJt7IeqA4Qkzxt2J3WbKC8Gg93Ng9VOg1Q&notify_self=1";
         String expectedMessage = "Hello websocket";
 
         context.setURI(url);
