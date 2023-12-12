df2981bc396032d8d9499dbc05682356aab86928
Yura Kochubailo <yurii.kochubailo@chisw.com>
2023-12-12 19:00:57 +0100

    add log

--- src/main/java/websocket/Client.java
+++ src/main/java/websocket/Client.java
@@ -1,5 +1,8 @@
 package websocket;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
 
@@ -8,6 +11,7 @@ import java.net.URISyntaxException;
 import java.util.Date;
 
 public class Client extends WebSocketClient {
+    private static final Logger logger = LogManager.getLogger(Client.class);
 
     private final SocketContext context;
 
@@ -19,7 +23,7 @@ public class Client extends WebSocketClient {
         this.context = context;
     }
 
-    public int getAliveTime(){
+    public int getAliveTime() {
         Date closeDate = new Date();
         int timeInSeconds = (int) (closeDate.getTime() - openedTime.getTime()) / 1000;
         context.setTimeTaken(timeInSeconds);
@@ -30,30 +34,29 @@ public class Client extends WebSocketClient {
     @Override
     public void onOpen(ServerHandshake handshakedata) {
         openedTime = new Date();
-        System.out.println("Opened Connection " + context.getURI());
+        logger.info("Opened Connection {}", context.getURI());
     }
 
 
     @Override
     public void onMessage(String message) {
-        System.out.println("Received new message " + message);
+        logger.info("Received new message {}", message);
         context.getMessageList().add(message);
         String expectedMessage = context.getExpectedMessage();
-        if(expectedMessage != null && expectedMessage.equals(message)){
-            closeConnection(1000,"Received expected message");
+        if (expectedMessage != null && expectedMessage.equals(message)) {
+            closeConnection(1000, "Received expected message");
         }
     }
 
     @Override
     public void onClose(int code, String reason, boolean remote) {
-        System.out.println("Close socket with code " + code + ", reason is " + reason);
+        logger.info("Close socket with code {}, reason is {}", code, reason);
         context.setStatusCode(code);
     }
 
     @Override
     public void onError(Exception ex) {
-
+        logger.error("WebSocket connection error", ex);
     }
-
 
 }
